0ed90455c914ff3dcf133257625f8936563250c6
Hein12345 <heinzawphyoe2@gmail.com>
2025-02-01 18:25:59 +0700

    feat: database changes

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -1,41 +1,46 @@
 <?php
-  
+
 namespace App\Http\Controllers;
-  
+
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use Validator;
-  
-  
+
+
 class AuthController extends Controller
 {
- 
+
     /**
      * Register a User.
      *
      * @return \Illuminate\Http\JsonResponse
      */
-    public function register() {
+    public function register()
+    {
         $validator = Validator::make(request()->all(), [
             'name' => 'required',
             'email' => 'required|email|unique:users',
             'password' => 'required|confirmed|min:8',
         ]);
-  
-        if($validator->fails()){
+
+        if ($validator->fails()) {
             return response()->json($validator->errors()->toJson(), 400);
         }
-  
+
         $user = new User;
         $user->name = request()->name;
         $user->email = request()->email;
         $user->password = bcrypt(request()->password);
+        $user->phone = request()->phone;
+        $user->address = request()->address;
+        $user->role =request()->role;
+        $user->date_of_birth = request()->date_of_birth;
         $user->save();
-  
+
         return response()->json($user, 201);
     }
-  
-  
+
+
     /**
      * Get a JWT via given credentials.
      *
@@ -44,14 +49,14 @@ class AuthController extends Controller
     public function login()
     {
         $credentials = request(['email', 'password']);
-  
+
         if (! $token = auth()->attempt($credentials)) {
             return response()->json(['error' => 'Unauthorized'], 401);
         }
-  
+
         return $this->respondWithToken($token);
     }
-  
+
     /**
      * Get the authenticated User.
      *
@@ -61,7 +66,7 @@ class AuthController extends Controller
     {
         return response()->json(auth()->user());
     }
-  
+
     /**
      * Log the user out (Invalidate the token).
      *
@@ -70,10 +75,10 @@ class AuthController extends Controller
     public function logout()
     {
         auth()->logout();
-  
+
         return response()->json(['message' => 'Successfully logged out']);
     }
-  
+
     /**
      * Refresh a token.
      *
@@ -83,7 +88,7 @@ class AuthController extends Controller
     {
         return $this->respondWithToken(auth()->refresh());
     }
-  
+
     /**
      * Get the token array structure.
      *
@@ -96,7 +101,7 @@ class AuthController extends Controller
         return response()->json([
             'access_token' => $token,
             'token_type' => 'bearer',
-            'expires_in' => auth()->factory()->getTTL() * 60
+            'expires_in' => auth()->factory()->getTTL() * 60 * 24
         ]);
     }
 }
--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,6 +17,10 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('phone');
+            $table->string('address');
+            $table->string('role');
+            $table->string('date_of_birth');
             $table->rememberToken();
             $table->timestamps();
         });
